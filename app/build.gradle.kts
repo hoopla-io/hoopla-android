49b514bde99bcfd1e72461d5807a134f242b1d0f
alisherjorayev070 <jorayevalisher070@gmail.com>
2026-02-27 19:09:27 +0500

    logo updated, version updated to 21(1.3.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "uz.alphazet.hoopla"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.3.2"
+        versionCode = 21
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
